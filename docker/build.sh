e9a949de481748d260601cd3826c185b77d8297a
Robert Verst <robert@verst.eu>
2020-02-19 14:38:54 +0100

    Improve dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 
-docker build --label "version=$1" --tag "rverst/miabdns:$1" --tag "rverst/miabdns:latest" .
-docker push --tag "rverst/miabdns:$1" --tag "rverst/miabdns:latest"
+while getopts ":u:p:v:t:" opt; do
+  case $opt in
+    u) DOCKER_USERNAME="$OPTARG"
+    ;;
+    p) DOCKER_PASSWORD="$OPTARG"
+    ;;
+    v) VERSION="$OPTARG"
+    ;;
+    t) GITHUB_TOKEN="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+docker build --no-cache --build-arg VERSION=${VERSION} --build-arg TOKEN=${GITHUB_TOKEN} -t rverst/miabdns:${VERSION} -t rverst/miabdns:latest .
+
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+docker push "rverst/miabdns:$VERSION"
+docker push "rverst/miabdns:latest"
